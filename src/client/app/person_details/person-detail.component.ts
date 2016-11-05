781115393ad9f32c477ad03d71df2e8982a4ad8b
Malte Bucksch <malte.bucksch@gmail.com>
2016-11-05 14:01:05 +0100

    Create files for person detail component

---
+++ src/client/app/person_details/person-detail.component.ts
@@ -0,0 +1,14 @@
+import {Component} from "@angular/core";
+/**
+ * Created by wanur on 05/11/2016.
+ */
+
+@Component({
+  moduleId: module.id,
+  selector: 'person-detail',
+  templateUrl: 'person-detail.component.html',
+  styleUrls: ["person-detail.component.css"]
+})
+export class PersonDetailComponent {
+
+}
