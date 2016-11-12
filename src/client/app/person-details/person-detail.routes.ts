6576ad7510d9563916d6b7f5eed237fabf347971
Malte Bucksch <malte.bucksch@gmail.com>
2016-11-12 10:29:48 +0100

    Delete all old files

--- src/client/app/person-details/person-detail.routes.ts
+++
@@ -1,6 +0,0 @@
-import {Routes} from "@angular/router";
-import {PersonDetailComponent} from "./person-detail.component";
-
-export const PersonDetailRoutes: Routes = [
-  {path: 'detail/:id', component: PersonDetailComponent}
-];
