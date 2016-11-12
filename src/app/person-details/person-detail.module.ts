9e6019829b97da754497f4386264bbf2feb58360
Malte Bucksch <malte.bucksch@gmail.com>
2016-11-12 17:30:58 +0100

    Improve design

--- src/app/person-details/person-detail.module.ts
+++ src/app/person-details/person-detail.module.ts
@@ -4,8 +4,9 @@ import {CommonModule} from "@angular/common";
 import {PersonDetailComponent} from "./person-detail.component";
 import {PersonListService} from "../shared/person-list/person-list.service";
 import {SharedModule} from "../shared/shared.module";
+import {MaterialModule} from "@angular/material";
 @NgModule({
-  imports: [CommonModule, SharedModule],
+  imports: [CommonModule, SharedModule,  MaterialModule.forRoot()],
   declarations: [PersonDetailComponent],
   exports: [PersonDetailComponent],
   providers: [PersonListService]
