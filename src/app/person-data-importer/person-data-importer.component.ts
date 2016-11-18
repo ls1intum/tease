cb91ea5f7e4c7172271f45f4b2be810ec0fa1c2b
Malte Bucksch <malte.bucksch@gmail.com>
2016-11-18 17:30:08 +0100

    Try to integrate papaparse

--- src/app/person-data-importer/person-data-importer.component.ts
+++ src/app/person-data-importer/person-data-importer.component.ts
@@ -1,14 +1,30 @@
-import {Component} from "@angular/core";
+import {Component, OnInit} from "@angular/core";
 /**
  * Created by wanur on 18/11/2016.
  */
+///<reference path="./path/to/moment.d.ts" />
 
+declare var Papa: any;
 
 @Component({
   templateUrl: './person-data-importer.component.html',
   styleUrls: ['./person-data-importer.component.css'],
   selector: 'person-data-importer',
 })
-export class PersonDataImporterComponent {
+export class PersonDataImporterComponent implements OnInit {
+  ngOnInit(): void {
+     let fileReader = new FileReader();
+  }
 
+  onFileChanged(event){
+    let files = event.srcElement.files;
+    if(files.length != 1)return;
+
+    // Papa.parse(files[0], {
+    //   complete: function(results){
+    //     console.log(results);
+    //   }
+    // });
+    Papa.parse("d,d");
+  }
 }
