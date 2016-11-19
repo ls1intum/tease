575aaabc2d15b4a192ae06bacf2bec31e6b822c5
Malte Bucksch <malte.bucksch@gmail.com>
2016-11-19 14:51:46 +0100

    Make parser work

--- src/app/person-data-importer/person-data-importer.component.ts
+++ src/app/person-data-importer/person-data-importer.component.ts
@@ -2,9 +2,8 @@ import {Component, OnInit} from "@angular/core";
 /**
  * Created by wanur on 18/11/2016.
  */
-///<reference path="./path/to/moment.d.ts" />
+import Papa = require("papaparse");
 
-declare var Papa: any;
 
 @Component({
   templateUrl: './person-data-importer.component.html',
@@ -13,18 +12,29 @@ declare var Papa: any;
 })
 export class PersonDataImporterComponent implements OnInit {
   ngOnInit(): void {
-     let fileReader = new FileReader();
+     // let fileReader = new FileReader();
+
+    console.log("data: "+Papa.parse("d,dududu,d").data[0]);
+
+    Papa.parse("3,3,3", {
+      delimiter: ',',
+      comments: false,
+
+      step: function(results, p) {
+        p.abort();
+        console.log(results.data.length);
+      }
+    });
   }
 
   onFileChanged(event){
     let files = event.srcElement.files;
     if(files.length != 1)return;
 
-    // Papa.parse(files[0], {
-    //   complete: function(results){
-    //     console.log(results);
-    //   }
-    // });
-    Papa.parse("d,d");
+    Papa.parse(files[0], {
+      complete: function(results){
+        console.log(results);
+      }
+    });
   }
 }
---
+++ typings/index.d.ts
@@ -0,0 +1,1 @@
+/// <reference path="globals/papaparse/index.d.ts" />
