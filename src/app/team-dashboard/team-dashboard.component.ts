0053b314283ee95202add09ca10600121c389457
Malte Bucksch <malte.bucksch@gmail.com>
2016-11-28 08:54:34 +0100

    Create border around containers

--- src/app/team-dashboard/team-dashboard.component.ts
+++ src/app/team-dashboard/team-dashboard.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from "@angular/core";
 import {TeamService} from "../shared/layers/business-logic-layer/services/team.service";
 import {Team} from "../shared/models/team";
+import {Router} from "@angular/router";
 /**
  * Created by Malte Bucksch on 25/11/2016.
  */
@@ -15,13 +16,23 @@ import {Team} from "../shared/models/team";
 export class TeamDashboardComponent implements OnInit{
   private teams: Team[];
 
-  constructor(private teamService: TeamService){
+  constructor(private teamService: TeamService,
+  private router: Router){
 
   }
 
   ngOnInit(): void {
     this.teamService.readTeams().then((teams) => {
       this.teams = teams;
+
+      if (teams == undefined || teams.length == 0)
+        this.gotoImport();
     });
+  }
+
+
+  gotoImport() {
+    let link = ["/import"];
+    this.router.navigate(link);
   }
 }
