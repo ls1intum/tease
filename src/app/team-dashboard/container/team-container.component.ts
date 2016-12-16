e93c76d78a8268e2db85135e51ad60a73ddd7618
Malte Bucksch <malte.bucksch@gmail.com>
2016-12-16 16:24:19 +0100

    Add real statistics about devices and team size

--- src/app/team-dashboard/container/team-container.component.ts
+++ src/app/team-dashboard/container/team-container.component.ts
@@ -2,6 +2,7 @@ import {Component, Input, ViewContainerRef, Output, EventEmitter} from "@angular
 import {Team} from "../../shared/models/team";
 import {DialogService} from "../../shared/ui/dialog.service";
 import {Person} from "../../shared/models/person";
+import {DeviceType} from "../../shared/models/device";
 /**
  * Created by Malte Bucksch on 06/12/2016.
  */
@@ -16,6 +17,7 @@ export class TeamContainerComponent {
   @Input()
   private team: Team;
   private isStatisticsVisible = false;
+  public LocalDeviceType = DeviceType;
   @Output() personClose = new EventEmitter();
 
   constructor(private dialogService: DialogService,
@@ -27,6 +29,16 @@ export class TeamContainerComponent {
     this.dialogService.showPersonDetails(person,this.viewContainerRef).subscribe(result => {
       this.personClose.emit();
     });
+  }
+
+  getDeviceCount(device: DeviceType): number{
+    return this.team.persons
+      .map(person => this.getPersonDeviceCount(person,device))
+      .reduce((sum, count)=>sum+count);
+  }
+
+  private getPersonDeviceCount(person: Person, targetDevice: DeviceType): number {
+    return person.devices.filter(device => device.deviceType === targetDevice).length;
   }
 
   showStatistics(){
