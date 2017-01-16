175d71363a53339af12dbbed221ae8ac02fb061d
Malte Bucksch <malte.bucksch@gmail.com>
2017-01-16 11:08:09 +0100

    Improve color design with new theme

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import {Router} from "@angular/router";
 @Component({
   selector   : 'app',
   templateUrl: './app.component.html',
-  styleUrls: ["./app.component.css"]
+  styleUrls: ["./app.component.css","./app.component.scss"]
 })
 export class AppComponent {
   private buttonName: string = "Skip";
