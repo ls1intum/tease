917b3c371bb3289d948439e75bc0a6385cd4f83c
qusc <quirin.schweigert@tum.de>
2017-11-28 20:09:50 +0100

    Complete person pool statistics, Change colors of team priority distribution bars

--- node/app/src/app/dashboard/dashboard/dashboard.component.ts
+++ node/app/src/app/dashboard/dashboard/dashboard.component.ts
@@ -3,11 +3,14 @@ import {Team} from '../../shared/models/team';
 import {TeamService} from '../../shared/layers/business-logic-layer/team.service';
 import {DragulaService} from 'ng2-dragula';
 import {ExamplePersonPropertyCsvRemotePath} from '../../shared/constants/csv.constants';
-import {Person} from '../../shared/models/person';
+import {Gender, Person} from '../../shared/models/person';
 import {PersonDetailOverlayComponent} from '../person-detail-overlay/person-detail-overlay.component';
 import {OverlayService} from '../../overlay.service';
 import {ConstraintsOverlayComponent} from '../constraints-overlay/constraints-overlay.component';
-import {DashboardService} from "../dashboard.service";
+import {DashboardService} from '../dashboard.service';
+import {Skill, SkillLevel} from '../../shared/models/skill';
+import {Colors} from '../../shared/constants/color.constants';
+import {Device} from '../../shared/models/device';
 
 enum PersonPoolDisplayMode {
   Closed, OneRow, TwoRows, Full
@@ -19,7 +22,8 @@ enum PersonPoolDisplayMode {
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent implements OnInit {
-  teams: Team[]; /* teams without orphan team */
+  teams: Team[];
+  /* teams without orphan team */
   teamIndices: number[];
   orphanTeam: Team;
   personPoolDisplayMode: PersonPoolDisplayMode = PersonPoolDisplayMode.OneRow;
@@ -28,6 +32,13 @@ export class DashboardComponent implements OnInit {
   priorityDistributionLabels: [number, number][]; // label and space that it should take
 
   PersonPoolDisplayMode = PersonPoolDisplayMode;
+  SkillLevel = SkillLevel;
+  Math = Math;
+  Colors = Colors;
+  Skill = Skill;
+  console = console;
+  Device = Device;
+  Gender = Gender;
 
   constructor(private teamService: TeamService,
               private dragulaService: DragulaService,
@@ -64,7 +75,7 @@ export class DashboardComponent implements OnInit {
 
   public sortTeams() {
     this.teams.concat(this.orphanTeam).forEach(team => {
-      team.persons.sort((personA, personB) =>  personB.supervisorRating - personA.supervisorRating);
+      team.persons.sort((personA, personB) => personB.supervisorRating - personA.supervisorRating);
     });
 
     this.saveTeams();
@@ -99,7 +110,8 @@ export class DashboardComponent implements OnInit {
       person: person,
       onClose: () => this.saveTeams(),
       onNextPersonClicked: () => this.showPersonDetails(this.orphanTeam.persons[this.orphanTeam.persons.indexOf(person) + 1]),
-      onPreviousPersonClicked: () => this.showPersonDetails(this.orphanTeam.persons[this.orphanTeam.persons.indexOf(person) - 1])});
+      onPreviousPersonClicked: () => this.showPersonDetails(this.orphanTeam.persons[this.orphanTeam.persons.indexOf(person) - 1])
+    });
   }
 
   public isDataLoaded(): boolean {
@@ -109,7 +121,7 @@ export class DashboardComponent implements OnInit {
   openConstraintsDialog() {
     this.overlayService.displayComponent(
       ConstraintsOverlayComponent,
-      { onTeamsGenerated: () =>  this.loadSavedTeams(), displayWarning: !this.areAllTeamsEmpty()}
+      {onTeamsGenerated: () => this.loadSavedTeams(), displayWarning: !this.areAllTeamsEmpty()}
     );
   }
 
@@ -118,8 +130,9 @@ export class DashboardComponent implements OnInit {
   }
 
   private calculateTeamDistributionStatistics() {
+    const teams = this.teams.concat(this.orphanTeam);
     this.priorityDistributionStatistics = new Map();
-    this.teams.forEach(team =>
+    teams.forEach(team =>
       this.priorityDistributionStatistics.set(
         team,
         this.teams.map((_, i) => this.getNumberOfVotesForTeamForPriority(team, i))
@@ -127,22 +140,20 @@ export class DashboardComponent implements OnInit {
     );
 
     this.priorityDistributionLabels = [[0, 0]];
-    const numberOfPersons = this.teams.reduce((acc, team) => acc + team.persons.length, 0);
+    const numberOfPersons = teams.reduce((acc, team) => acc + team.persons.length, 0);
     const step = 10;
     for (let i = step; i < numberOfPersons; i += step)
       this.priorityDistributionLabels.push([i, step]);
     const lastStep = numberOfPersons - this.priorityDistributionLabels[this.priorityDistributionLabels.length - 1][0];
     this.priorityDistributionLabels.push([numberOfPersons, lastStep]);
+
+
+    console.log(this.priorityDistributionLabels);
+    console.log(this.priorityDistributionStatistics);
   }
 
   getNumberOfVotesForTeamForPriority(team: Team, priority: number): number {
-    return this.teams.reduce(
-      (totalMatchesAcc, curTeam) => totalMatchesAcc + curTeam.persons.reduce(
-        (matchesPerTeamAcc, curPerson) => matchesPerTeamAcc + (curPerson.teamPriorities[priority] === team ? 1 : 0),
-        0
-      ),
-      0
-    );
+    return this.getNumberOfPersonsWithPredicate((person) => person.teamPriorities[priority] === team);
   }
 
   togglePersonPoolStatistics() {
@@ -152,10 +163,47 @@ export class DashboardComponent implements OnInit {
   }
 
   getColorOfTeamDistributionBar(priority: number): string {
+    /*
+    // the following code fades between green and red
     const green = [117, 190, 117];
     const red = [209, 111, 111];
     const ratio = priority / (this.teams.length - 1);
     const color = green.map((greenComp, i) => Math.round((1 - ratio) * greenComp + ratio * red[i]));
     return `rgb(${color[0]}, ${color[1]}, ${color[2]})`;
+    */
+
+    if (priority < 3)
+      return Colors.getColor(SkillLevel.High);
+    else if (priority < 6)
+      return Colors.getColor(SkillLevel.Medium);
+    else
+      return Colors.getColor(SkillLevel.Low);
+  }
+
+  getNumberOfPersonsWithSupervisorRating(skillLevel: SkillLevel): number {
+    return this.getNumberOfPersonsWithPredicate((person) => person.supervisorRating === skillLevel);
+  }
+
+  getNumberOfPersons(): number {
+    return this.teams.concat(this.orphanTeam).reduce((acc, team) => acc + team.persons.length, 0);
+  }
+
+  onPersonPoolDisplayModeChange() {
+    if (this.personPoolDisplayMode !== PersonPoolDisplayMode.Full && this.statisticsVisible)
+      this.togglePersonPoolStatistics();
+  }
+
+  getTotalNumberOfDeviceType(device: Device): number {
+    return this.getNumberOfPersonsWithPredicate((person) => person.devices.includes(device));
+  }
+
+  getNumberOfPersonsWithGender(gender: Gender) {
+    return this.getNumberOfPersonsWithPredicate((person) => person.gender === gender);
+  }
+
+  private getNumberOfPersonsWithPredicate(predicate: (Person) => boolean): number {
+    return this.teams.concat(this.orphanTeam).reduce(
+      (totalMatchesAcc, curTeam) => totalMatchesAcc + curTeam.persons.reduce(
+        (matchesPerTeamAcc, curPerson) => matchesPerTeamAcc + (predicate(curPerson) ? 1 : 0), 0), 0);
   }
 }
