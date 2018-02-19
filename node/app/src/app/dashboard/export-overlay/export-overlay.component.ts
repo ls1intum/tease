e63377cc2088ff31d7b4dcb778610cf9fc1e06d4
qusc <quirin.schweigert@tum.de>
2018-02-19 21:59:49 +0100

    Improve person highlighting overlay; Implement screenshot prove of concept

--- node/app/src/app/dashboard/export-overlay/export-overlay.component.ts
+++ node/app/src/app/dashboard/export-overlay/export-overlay.component.ts
@@ -1,6 +1,10 @@
 import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
 import {OverlayComponent} from '../../overlay.service';
 import {TeamService} from '../../shared/layers/business-logic-layer/team.service';
+import * as html2canvas from 'html2canvas';
+import * as FileSaver from 'file-saver';
+import {PersonDetailOverlayComponent} from '../person-detail-overlay/person-detail-overlay.component';
+import {Person} from '../../shared/models/person';
 
 @Component({
   selector: 'app-export-overlay',
@@ -10,6 +14,9 @@ import {TeamService} from '../../shared/layers/business-logic-layer/team.service
 export class ExportOverlayComponent implements OnInit, OverlayComponent {
   public data: {};
 
+  @ViewChild(PersonDetailOverlayComponent) personDetailOverlayComponent: PersonDetailOverlayComponent;
+  @ViewChild(PersonDetailOverlayComponent, { read: ElementRef }) personDetailOverlayComponentRef: ElementRef;
+
   constructor(private teamService: TeamService) { }
   ngOnInit() {}
 
@@ -17,5 +24,33 @@ export class ExportOverlayComponent implements OnInit, OverlayComponent {
     this.teamService.saveToLocalBrowserStorage().then(success => {
       this.teamService.exportSavedState();
     });
+  }
+
+  exportScreenshots() {
+    this.personDetailOverlayComponent.data = {
+      person: this.teamService.persons[0],
+      onClose: () => {},
+      onPreviousPersonClicked: () => {},
+      onNextPersonClicked: () => {},
+      onPersonClicked: (person: Person) => {},
+    };
+
+    setTimeout(() => {
+      console.log(this.personDetailOverlayComponentRef);
+
+      const options = {
+        allowTaint: false,
+        backgroundColor: null,
+        scale: 2.0,
+        useCORS: true
+
+      };
+
+      html2canvas(this.personDetailOverlayComponentRef.nativeElement, options).then(canvas => {
+        canvas.toBlob(function(blob) {
+          FileSaver.saveAs(blob, 'screenshot.png');
+        });
+      });
+    }, 1000);
   }
 }
